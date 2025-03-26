35de9e4100c170974ba15ce177989f934b5f3711
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2025-03-26 18:20:14 +0430

    add user and context

---
+++ Resume2.Data/Context/Resume2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Resume2.Domain.Models.Auth;
+
+namespace Resume2.Data.Context
+{
+    public class Resume2Context : DbContext
+
+    {
+        public Resume2Context(DbContextOptions<Resume2Context> options):base(options) 
+        {
+            
+        }
+        public DbSet<User> users { get; set; }
+    }
+}
---
+++ Resume2.Domain/Models/Auth/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume2.Domain.Models.Auth
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName{ get; set; }
+        public string Password { get; set; }
+    }
+}
