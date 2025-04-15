7db0ede5ad31e2a56ac83ae41a31d6b41edb3f34
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2025-04-15 18:34:19 +0430

    doc2

--- Resume2.Domain/Models/Web/WebDocType.cs
+++ Resume2.Domain/Models/Web/WebDocType.cs
@@ -12,8 +12,8 @@ namespace Resume2.Domain.Models.Web
     {
         [Key]
         public int Id { get; set; }
-        public string Type { get; set; }
-        public string Description { get; set; }
+        public string Title { get; set; }
+        public string Title_En  { get; set; }
 
         #region Relation
         public List<WebDocDetails> webDocDetails { get; set; }
