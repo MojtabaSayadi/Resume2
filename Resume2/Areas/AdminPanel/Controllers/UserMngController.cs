0aeeaf937631ce142d47941797ada15854d80883
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2025-05-27 17:46:19 +0430

    view components

---
+++ Resume2/Areas/AdminPanel/Controllers/UserMngController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume2.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class UserMngController : Controller
+    {
+        [HttpGet("UserMngDashboard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Resume2/Areas/AdminPanel/Views/ViewComponents/LoginStatusComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume2.Areas.AdminPanel.Views.ViewComponents
+{
+    public class LoginStatusComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("LoginStatusDashboard"));
+
+        }
+
+    }
+}
