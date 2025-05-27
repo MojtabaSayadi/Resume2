fbc515439b18e41dc7290b48da014f0e6eeb3854
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2025-05-27 11:35:39 +0430

    start admin panel

---
+++ Resume2/Areas/AdminPanel/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume2.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class AdminController : Controller
+    {
+        [HttpGet("AdminDashboard")]
+        public IActionResult IndexAdmin()
+        {
+            return View();
+        }
+    
+    }
+}
